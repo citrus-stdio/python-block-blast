b8ae0d5d58361b9f7500c81dd501fb080b7938de
citrus-stdio <cyruschews@gmail.com>
2026-01-03 15:20:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class block:
         self.length = l
 
 
-        
+score = 0
 lblock1 = block([[1,0],[1,0],[1,1]],2,3)
 lblock2 = block([[0,1],[0,1],[1,1]],2,3)
 lblock3 = block([[1,1],[1,0],[1,0]],2,3)
@@ -35,64 +35,90 @@ tblock4 = block([[1,0],[1,1],[1,0]],2,3)
 board = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
 xstrip = [["0","1","2","3","4","5","6","7"]]
 ystrip = [["0"],["1"],["2"],["3"],["4"],["5"],["6"],["7"]]
-blocks = [lblock1,lblock2,lblock3,lblock4,smallsquare,bigsquare,corner1,corner2,corner3,corner4,zig1,zig2,zag1,zag2,line1,line2,line3,line4,tblock1,tblock2,tblock3,tblock4]
+blocklist = [lblock1,lblock2,lblock3,lblock4,smallsquare,bigsquare,corner1,corner2,corner3,corner4,zig1,zig2,zag1,zag2,line1,line2,line3,line4,tblock1,tblock2,tblock3,tblock4]
 blocklistx = 12
 blocklisty = 5
+scoreboard = [["S","C","O","R","E",":"," ",str(score)]]
 
 render = fillRender(30,10)
 place(render,xstrip,1,0)
 place(render,ystrip,0,1)
-
-#while True:
+place(render,scoreboard,14,1)
 
 # Behold the texturizer
-texturize(blocks,"x")
-# Block selection
-block1 = random.choice(blocks)
-block2 = random.choice(blocks)
-block3 = random.choice(blocks)
-# block dictionary!!
-blocks = {
-    "1": [block1.shape, True],
-    "2": [block2.shape, True],
-    "3": [block3.shape, True]
-}
-# Put block choice on the screen
-place(render,block1.shape,blocklistx,blocklisty)
-place(render,[[","]],blocklistx+1+block1.width,blocklisty)
-place(render,[["1"]],blocklistx,blocklisty-2)
-place(render,block2.shape,blocklistx+3+block1.width,blocklisty)
-place(render,[[","]],blocklistx+4+block1.width+block2.width,blocklisty)
-place(render,[["2"]],blocklistx+4+block1.width,blocklisty-2)
-place(render,block3.shape,blocklistx+6+block1.width+block2.width,blocklisty)
-place(render,[["3"]],blocklistx+6+block1.width+block2.width,blocklisty-2)
+blocklist = texturize(blocklist, "x")
 
-# Display board so that player can see before placing blocks :)
-place(render,board,1,1)
-displayRender()
+while True:
 
-for i in range(0,3):
+    print('loop start')
+    # Block selection
+    block1 = random.choice(blocklist)
+    block2 = random.choice(blocklist)
+    block3 = random.choice(blocklist)
+    # block dictionary!!
+    blocks = {
+        "1": [block1.shape, True],
+        "2": [block2.shape, True],
+        "3": [block3.shape, True]
+    }
 
-    # Get player input
-    while True:
-        try:
-            playerInput = getPlayerInput(board,blocks)
-            break
-        except(Exception) as e:
-            # specified instruction error
-            print(e)
+    # reset render
+    render = fillRender(30, 10)
+    place(render, xstrip, 1, 0)
+    place(render, ystrip, 0, 1)
+    place(render, scoreboard, 14, 1)
 
-    # place block on the board
-    place(playerInput[0],playerInput[1],playerInput[2],playerInput[3])
-    # Clear block that is used from screen
-    clearChoice(playerInput[4],block1,block2,block3,blocklistx,blocklisty)
+    # Put block choice on the screen
+    place(render,block1.shape,blocklistx,blocklisty)
+    place(render,[[","]],blocklistx+1+block1.width,blocklisty)
+    place(render,[["1"]],blocklistx,blocklisty-2)
+    place(render,block2.shape,blocklistx+3+block1.width,blocklisty)
+    place(render,[[","]],blocklistx+4+block1.width+block2.width,blocklisty)
+    place(render,[["2"]],blocklistx+4+block1.width,blocklisty-2)
+    place(render,block3.shape,blocklistx+6+block1.width+block2.width,blocklisty)
+    place(render,[["3"]],blocklistx+6+block1.width+block2.width,blocklisty-2)
 
-    # Place board on render again and display the render
+    # Display board so that player can see before placing blocks :)
     place(render,board,1,1)
     displayRender()
 
-    blocks[str(playerInput[4])][1] = False
-    
-    #if input("press 0 to continue: ") != str(0):
-        #break
+    for i in range(0,3):
+
+        # Get player input
+
+        while True:
+            try:
+                playerInput = getPlayerInput(board,blocks)
+                break
+            except(Exception) as e:
+                # specified instruction error
+                print(e)
+
+        # place block on the board
+        place(playerInput[0],playerInput[1],playerInput[2],playerInput[3])
+        # Clear block that is used from screen
+        clearChoice(playerInput[4],block1,block2,block3,blocklistx,blocklisty)
+        # clear lines
+        cleared = clear(board)
+        board = cleared[0]
+        score += cleared[1]
+        # print the scoreboard
+        place(render, scoreboard, 14, 1)
+
+        # reset render
+        fill(render,1,1,9,9)
+
+        # Place board on render again and display the render
+        place(render,board,1,1)
+        displayRender()
+
+        # make the block just chosen unavailable
+        blocks[str(playerInput[4])][1] = False
+
+        # check for lose state
+        #if loseState(board,blocks):
+         #   print("you lost")
+
+        #if input("press 0 to continue: ") != str(0):
+            #break
 
